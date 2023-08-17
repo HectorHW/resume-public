3126e7be85aaab3dc0e13acca76ca07f1aed0ccb
Vladimir Redkin <vsredkin01@gmail.com>
2023-08-17 15:47:59 +0300

    use trap for proper cleanup

--- update-resume.sh
+++ update-resume.sh
@@ -3,8 +3,6 @@
 # this script is intended to be run by webhook server
 # $1 is workflow id
 
-set -e
-
 # we have to wait for gh actions to finish and nightly.link to update
 sleep 5m
 
@@ -12,6 +10,7 @@ TEMPD=$(mktemp -d)
 
 trap "exit 1"           HUP INT PIPE QUIT TERM
 trap 'rm -rf "$TEMPD"'  EXIT
+trap 'exit 1' ERR
 
 cd $TEMPD
 
